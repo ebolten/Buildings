5aacae98b08733290f4dc8e0c421a5a0eb638dd7
Emily Bolten <44903354+ebolten@users.noreply.github.com>
2019-02-28 19:39:44 -0500

    Add files via upload

---
+++ edificios.py
@@ -0,0 +1,93 @@
+import turtle
+
+
+my_turtle = turtle.Turtle()
+my_turtle.speed(20)
+
+#This part of the code draws a square.
+def square():
+    for i in range(4):
+        my_turtle.forward(100)
+        my_turtle.left(90)
+
+#This is a scene of buildings
+my_turtle.left(20)
+my_turtle.forward(500)
+my_turtle.back(500)
+my_turtle.right(40)
+my_turtle.back(500)
+my_turtle.forward(500)
+my_turtle.left(110)
+my_turtle.forward(100)
+my_turtle.left(70)
+my_turtle.forward(158)
+my_turtle.left(110)
+my_turtle.forward(100)
+my_turtle.left(70)
+my_turtle.forward(158)
+my_turtle.right(140)
+my_turtle.back(158)
+my_turtle.right(110)
+my_turtle.forward(100)
+my_turtle.left(110)
+my_turtle.forward(158)
+my_turtle.back(158)
+my_turtle.right(40)
+my_turtle.forward(158)
+my_turtle.left(40)
+my_turtle.forward(158)
+my_turtle.left(70)
+my_turtle.back(400)
+my_turtle.forward(400)
+my_turtle.left(110)
+my_turtle.forward(158)
+my_turtle.left(70)
+my_turtle.forward(300)
+my_turtle.back(300)
+my_turtle.right(70)
+my_turtle.left(180)
+my_turtle.forward(158)
+my_turtle.left(70)
+my_turtle.forward(100)
+my_turtle.left(180)
+my_turtle.left(70)
+my_turtle.forward(95)
+my_turtle.left(110)
+my_turtle.back(400)
+my_turtle.forward(400)
+my_turtle.left(70)
+my_turtle.forward(253)
+my_turtle.left(40)
+my_turtle.forward(158)
+my_turtle.left(70)
+my_turtle.forward(180)
+my_turtle.left(70)
+my_turtle.forward(158)
+my_turtle.back(158)
+my_turtle.left(220)
+my_turtle.forward(170)
+my_turtle.left(140)
+my_turtle.forward(170)
+my_turtle.left(40)
+my_turtle.forward(163)
+my_turtle.back(163)
+my_turtle.left(140)
+my_turtle.forward(170)
+my_turtle.left(290)
+my_turtle.forward(180.5)
+my_turtle.right(70)
+my_turtle.forward(328.35)
+my_turtle.right(40)
+my_turtle.forward(350)
+my_turtle.left(290)
+my_turtle.forward(300)
+
+
+
+
+
+
+
+
+
+
